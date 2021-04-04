1ab6264e224c7bc940a7481e277190ac19e0b19e
GalkinDenis <72812069+GalkinDenis@users.noreply.github.com>
2021-04-05 00:06:25 +0700

    Update favorites.java

--- app/src/main/java/com/example/financeapp/favorites.java
+++ app/src/main/java/com/example/financeapp/favorites.java
@@ -1,10 +1,13 @@
 package com.example.financeapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,82 +19,86 @@ import java.util.Map;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
-
 //Актитвити закладки фавориты.
 public class favorites extends Activity {
 
-    LayoutInflater ltInflater;
-    LinearLayout linLayout;
-    ImageView label;
+    TextView name;
+    TextView index;
     TextView price;
-    TextView changing;
-    ImageView upOrDown;
     TextView ticker;
+    ImageView label;
+    TextView changing;
+    Button showDetails;
+    ImageView upOrDown;
+    Animation animAlpha;
+    LinearLayout linLayout;
+    LayoutInflater ltInflater;
     Button removeFromFavorites;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        //Создание потока под запрос на Yahoo finance, для получения акции.
-        new Thread(){
-            @Override
-            public void run() {
-                Stock stock = null;
-                for (int i = 0; i < tickerPlusLabel.tickerList.size(); i++) {
+        //Вывод содержимого избранного(кеша) на экран.
+        for (int i = 0; i < tickerPlusLabel.tickerList.size(); i++) {
+            linLayout = (LinearLayout) findViewById(R.id.linLayout);
 
-                    int finalI = i;
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
+            ltInflater = getLayoutInflater();
+            View item = ltInflater.inflate(R.layout.item, linLayout, false);
 
-                            linLayout = (LinearLayout) findViewById(R.id.linLayout);
+            label = (ImageView) item.findViewById(R.id.imageTesla);
+            label.setImageResource(tickerPlusLabel.choiceLabel(tickerPlusLabel.tickerList.get(i)));
 
-                            ltInflater = getLayoutInflater();
-                            View item = ltInflater.inflate(R.layout.item, linLayout, false);
+            ticker = (TextView) item.findViewById(R.id.ticker);
+            ticker.setText("(" + tickerPlusLabel.tickerList.get(i) + ")");
 
-                            label = (ImageView) item.findViewById(R.id.imageTesla);
-                            label.setImageResource(tickerPlusLabel.choiceLabel(tickerPlusLabel.tickerList.get(finalI)));
+            name = (TextView) item.findViewById(R.id.name);
+            name.setText(tickerPlusLabel.stockNameStack.get(tickerPlusLabel.tickerList.get(i)));
 
-                            ticker = (TextView) item.findViewById(R.id.ticker);
-                            ticker.setText(tickerPlusLabel.tickerList.get(finalI));
+            price = (TextView) item.findViewById(R.id.price);
+            price.setText("$ " + tickerPlusLabel.priceList.get(i));
 
-                            price = (TextView) item.findViewById(R.id.price);
-                            price.setText("$ " + tickerPlusLabel.priceList.get(finalI));
+            index = (TextView) item.findViewById(R.id.index);
+            index.setText(tickerPlusLabel.changeIndexList.get(i));
 
-                            changing = (TextView) item.findViewById(R.id.changing);
-                            changing.setText(tickerPlusLabel.changeList.get(finalI));
+            changing = (TextView) item.findViewById(R.id.changing);
+            changing.setText("(" + tickerPlusLabel.changeList.get(i) + "%)");
 
-                            //Вывод зеленой\красной стрелки, в зависимости от знака перед значением.
-                            upOrDown = (ImageView) item.findViewById(R.id.upOrDown);
-                            double converter = Double.parseDouble(tickerPlusLabel.changeList.get(finalI));
-                            if (converter > 0){
-                                upOrDown.setScaleY(1);
-                                upOrDown.setScaleX(1);
-                                upOrDown.setImageResource(R.drawable.gr);
-                            } else {
-                                upOrDown.setScaleY(1);
-                                upOrDown.setScaleX(1);
-                                upOrDown.setImageResource(R.drawable.r);
-                            }
 
-                            //Кнопка удаления позиции из избранного.
-                            removeFromFavorites = (Button) item.findViewById(R.id.favorits);
-                            removeFromFavorites.setText(R.string.removeFromFavorites);
-                            removeFromFavorites.setTag(tickerPlusLabel.tickerList.get(finalI));
-
-                            linLayout.addView(item);
-                        }
-                    });
-                }
+            //Вывод зеленой\красной стрелки, в зависимости от знака перед значением.
+            upOrDown = (ImageView) item.findViewById(R.id.upOrDown);
+            double converter = Double.parseDouble(tickerPlusLabel.changeList.get(i));
+            if (converter > 0) {
+                upOrDown.setScaleY(1);
+                upOrDown.setScaleX(1);
+                upOrDown.setImageResource(R.drawable.gr);
+            } else {
+                upOrDown.setScaleY(1);
+                upOrDown.setScaleX(1);
+                upOrDown.setImageResource(R.drawable.r);
             }
-        }.start();
+
+            showDetails = (Button) item.findViewById(R.id.details);
+            showDetails.setText(R.string.showDetails);
+            showDetails.setTag(tickerPlusLabel.tickerList.get(i));
+
+            //Кнопка удаления позиции из избранного.
+            removeFromFavorites = (Button) item.findViewById(R.id.favorits);
+            removeFromFavorites.setText(R.string.removeFromFavorites);
+            removeFromFavorites.setTag(tickerPlusLabel.tickerList.get(i));
+
+            //Добавление элемента в список.
+            linLayout.addView(item);
+        }
     }
 
     @Override
@@ -103,19 +110,41 @@ public class favorites extends Activity {
         }
     }
 
-    public void onClick(View v){
-        //Удаления позиции из избранного(кеша).
-            int index = tickerPlusLabel.tickerList.indexOf((String) v.getTag());
+    //Вывод активити с деталями найденной акции.
+    public void onClickShowDetails(View v){
+        v.startAnimation(animAlpha);
 
-            tickerPlusLabel.tickerList.remove(index);
-            tickerPlusLabel.priceList.remove(index);
-            tickerPlusLabel.changeList.remove(index);
-            tickerPlusLabel.dividentList.remove(index);
+        int index = tickerPlusLabel.tickerList.indexOf((String) v.getTag());
+
+        //Вывод активити с деталями найденной акции.
+        Intent intentFindStock = new Intent("showDetailsFromCache");
+        intentFindStock.putExtra("ticker", tickerPlusLabel.tickerList.get(index));
+        intentFindStock.putExtra("price", tickerPlusLabel.priceList.get(index));
+        intentFindStock.putExtra("change", tickerPlusLabel.changeList.get(index));
+        intentFindStock.putExtra("changeIndex", tickerPlusLabel.changeIndexList.get(index));
+        intentFindStock.putExtra("divident", tickerPlusLabel.dividentList.get(index));
+        startActivity(intentFindStock);
+    }
+
+    //Удаление из избанного(кеша).
+    public void onClickAddOrRemoveFromFavorites(View v){
+        v.startAnimation(animAlpha);
+
+        //Удаления позиции из избранного(кеша).
+        int index = tickerPlusLabel.tickerList.indexOf((String) v.getTag());
+
+        tickerPlusLabel.tickerList.remove(index);
+        tickerPlusLabel.priceList.remove(index);
+        tickerPlusLabel.changeList.remove(index);
+        tickerPlusLabel.changeIndexList.remove(index);
+        tickerPlusLabel.dividentList.remove(index);
 
         if(linLayout != null) {
             //Обнуление списка.
             linLayout.removeAllViews();
         }
+
+        //Перерисовка обновленного списка.
         onResume();
     }
 
